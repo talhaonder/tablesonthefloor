ceaf825cb4b9d0f2ff51f6d52f8e9b1e448b7913
talhanzo <ltalhaonderl@gmail.com>
2023-08-09 09:58:20 +0300

    connectwithdb

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Member
 
 # Create your views here.
+def index(request):
+    all_members = Member.objects.all()
+    return render(request, 'datatables/index.html', {'all_members': all_members})
--- testdev/urls.py
+++ testdev/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from myapp import views
 
 urlpatterns = [
+    path('index/', views.index, name='index'),
     path('admin/', admin.site.urls),
 ]
